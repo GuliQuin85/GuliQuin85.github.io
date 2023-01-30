0660a7d630ca85f127deb7f819a568c8a7d6ba67
Gonzalo Quintana <gjquintana.gq@gmail.com>
2023-01-30 01:27:08 -0300

    Versión Final 1

--- js/function.js
+++ js/function.js
@@ -1,20 +1,22 @@
 
-document.getElementById("boton_atras").addEventListener("click", function () {
-    console.log("Deshacer cambios");
-    document.body.style.backgroundColor = "blue";
-    document.getElementById("demo").innerHTML = "Aprendiendo JavaScript";
-    document.getElementById("demo").style = "/*display = none*/";
+document.getElementById("modo_oscuro").addEventListener("click", function () {
+    console.log("Cambiar colores");
+    document.body.style.backgroundColor = "black";
 });
+
+
 
 window.addEventListener('load', iniciar, false);
 
 function iniciar() {      
   var imagen = document.getElementById('linkedin'), original = imagen.src;
   
-  imagen.addEventListener('mouseover', function(){
-      this.src = "imagenes/linkedin.png";
-  }, false);
-  imagen.addEventListener('mouseout', function(){
-      this.src = original;
-  }, false);
+    imagen.addEventListener('mouseover', function(){
+        this.src = "imagenes/linkedin.png";
+        this.srcset = "imagenes/linkedin.png 800w";
+    }, false);
+    imagen.addEventListener('mouseout', function(){
+        this.src = original;
+        this.srcset = "imagenes/Logo-LinkedIn-1.jpg 800w";
+    }, false);
 }
